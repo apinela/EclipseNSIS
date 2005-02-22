b1a292d8d6edf293e5f5854feb5dcf53b368d4dc
icemank <>
2005-02-22 03:14:57 +0000

    *** empty log message ***

--- WinAPI/build.bat
+++ WinAPI/build.bat
@@ -1,6 +1,6 @@
 @echo off
 rem *******************************************************************************
-rem * Copyright (c) 2004 Sunil Kamath (IcemanK).
+rem * Copyright (c) 2004, 2005 Sunil Kamath (IcemanK).
 rem * All rights reserved. This program and the accompanying materials 
 rem * are made available under the terms of the Common Public License v1.0
 rem * which is available at http://www.eclipse.org/legal/cpl-v10.html
