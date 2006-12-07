9b8762f3ce60b79d6f007f70486c78a340cebedf
icemank <>
2006-12-07 00:33:54 +0000

    *** empty log message ***

--- net.sf.eclipsensis.update/src/net/sf/eclipsensis/update/jobs/DownloadURLsParserCallback.java
+++ net.sf.eclipsensis.update/src/net/sf/eclipsensis/update/jobs/DownloadURLsParserCallback.java
@@ -3,68 +3,102 @@
  * All rights reserved.
  * This program is made available under the terms of the Common Public License
  * v1.0 which is available at http://www.eclipse.org/legal/cpl-v10.html
- * 
+ *
  * Contributors:
  *     Sunil Kamath (IcemanK) - initial API and implementation
  *******************************************************************************/
 package net.sf.eclipsensis.update.jobs;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.*;
+import java.util.*;
 
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.HTML.Attribute;
 import javax.swing.text.html.HTML.Tag;
 import javax.swing.text.html.HTMLEditorKit.ParserCallback;
 
-import net.sf.eclipsensis.util.Common;
+import net.sf.eclipsensis.update.EclipseNSISUpdatePlugin;
+import net.sf.eclipsensis.update.net.NetworkUtil;
+import net.sf.eclipsensis.util.IOUtility;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 
 class DownloadURLsParserCallback extends ParserCallback
 {
-    private static final String ID_MIRRORS = "mirrors"; //$NON-NLS-1$
-    
-    private static final int TD_NONE = -1;
-    private static int COUNT=0;
-    private static final int TD_HOST = COUNT++;
-    private static final int TD_LOCATION = COUNT++;
-    private static final int TD_CONTINENT = COUNT++;
-    private static final int TD_DOWNLOAD = COUNT++;
-    
-    private Pattern mPattern;
-    private boolean mSearching = true;
-    private boolean mInTD = false;
-    private int mExpectedTD = TD_NONE;
+    private static final String PROPERTIES_FILE_NAME = "siteimages.properties"; //$NON-NLS-1$
+    private static final IPath cLocalPropertiesPath = new Path("/resources/"+PROPERTIES_FILE_NAME); //$NON-NLS-1$
+
+    private static final String FORM_ACTION = "/project/downloading.php"; //$NON-NLS-1$
+    private static final String USE_MIRROR = "use_mirror"; //$NON-NLS-1$
+    private static final String AUTO_SELECT = "Auto-select"; //$NON-NLS-1$
+    private static final String TAG_LABEL = "label"; //$NON-NLS-1$
+    private static final String INPUT_RADIO = "radio"; //$NON-NLS-1$
+
+    private static long cTimestamp = 0;
+    private static Properties cImageNames = new Properties();
+
+    private boolean mDone = false;
+    private boolean mInForm = false;
+    private boolean mInSpan = false;
+    private boolean mInLabel = false;
+    private String mContinent = null;
+
     private String[] mCurrentSite  = null;
     private List mSites = new ArrayList();
-    
-    public DownloadURLsParserCallback(String version)
+
+    private static void loadImageNames()
     {
-        mPattern = Pattern.compile(new StringBuffer("/nsis/nsis\\-").append( //$NON-NLS-1$
-                        version.replaceAll("\\.","\\.")).append( //$NON-NLS-1$ //$NON-NLS-2$
-                        "\\-setup\\.exe\\?use\\_mirror=(\\w+)").toString(),Pattern.CASE_INSENSITIVE); //$NON-NLS-1$
+        InputStream is = null;
+        try {
+            File file = new File(EclipseNSISUpdatePlugin.getPluginStateLocation(),PROPERTIES_FILE_NAME);
+            if(!NetworkUtil.downloadLatest(NSISUpdateURLs.getSiteImagesUpdateURL(),file)) {
+                file = IOUtility.ensureLatest(EclipseNSISUpdatePlugin.getDefault().getBundle(),cLocalPropertiesPath,EclipseNSISUpdatePlugin.getPluginStateLocation());
+            }
+
+            long timestamp = file.lastModified();
+            if(timestamp > cTimestamp) {
+                cTimestamp = timestamp;
+                is = new FileInputStream(file);
+                cImageNames.clear();
+                cImageNames.load(is);
+            }
+        }
+        catch (IOException e) {
+            EclipseNSISUpdatePlugin.getDefault().log(e);
+        }
+        finally {
+            IOUtility.closeIO(is);
+        }
+    }
+
+    public DownloadURLsParserCallback()
+    {
+        loadImageNames();
     }
 
     public void handleEndTag(Tag t, int pos)
     {
-        if (mSearching) {
-            if(t.equals(Tag.TABLE)) {
-                mExpectedTD = TD_NONE;
+        if(!mDone) {
+            if(t.equals(Tag.FORM)) {
+                if(mInForm) {
+                    mInForm = false;
+                    mDone = true;
+                }
             }
-            else if(t.equals(Tag.TD) && mExpectedTD != TD_NONE) {
-                mExpectedTD = ++mExpectedTD % (COUNT);
-                mInTD = false;
-                if(mExpectedTD == 0 && mCurrentSite != null) {
-                    boolean canAdd = true;
-                    for (int i = 0; i < mCurrentSite.length; i++) {
-                        if(Common.isEmpty(mCurrentSite[i])) {
-                            canAdd = false;
-                            break;
+            else if(t.equals(Tag.SPAN)) {
+                if(mInForm) {
+                    mInSpan = false;
+                }
+            }
+            else if(t.toString().equals(TAG_LABEL)) {
+                if(mInForm) {
+                    mInLabel = false;
+                    if(mCurrentSite != null) {
+                        if(mCurrentSite[3] != null) {
+                            mSites.add(mCurrentSite);
                         }
-                    }
-                    if(canAdd) {
-                        mSites.add(mCurrentSite);
+                        mCurrentSite = null;
                     }
                 }
             }
@@ -73,10 +107,41 @@ class DownloadURLsParserCallback extends ParserCallback
 
     public void handleSimpleTag(Tag t, MutableAttributeSet a, int pos)
     {
-        if (mSearching && mInTD) {
-            if(t.equals(Tag.IMG) && mExpectedTD == TD_HOST) {
-                if(a.isDefined(Attribute.SRC)) {
-                    mCurrentSite[TD_HOST] = (String)a.getAttribute(Attribute.SRC);
+        if(!mDone) {
+            if(t.equals(Tag.SPAN)) {
+                if(mInForm) {
+                    if(mInSpan) {
+                        handleEndTag(t, pos);
+                    }
+                    else {
+                        handleStartTag(t, a, pos);
+                    }
+                }
+            }
+            else if(t.toString().equals(TAG_LABEL)) {
+                if(mInForm) {
+                    if(mInLabel) {
+                        handleEndTag(t, pos);
+                    }
+                    else {
+                        handleStartTag(t, a, pos);
+                    }
+                }
+            }
+            else if(t.equals(Tag.INPUT)) {
+                if(mInForm && mInLabel && mCurrentSite != null) {
+                    if(a.isDefined(Attribute.TYPE)) {
+                        if(INPUT_RADIO.equalsIgnoreCase((String)a.getAttribute(Attribute.TYPE))) {
+                            if(a.isDefined(Attribute.NAME)) {
+                                if(USE_MIRROR.equalsIgnoreCase((String)a.getAttribute(Attribute.NAME))) {
+                                    if(a.isDefined(Attribute.VALUE)) {
+                                        mCurrentSite[3] = (String)a.getAttribute(Attribute.VALUE);
+                                        mCurrentSite[0] = cImageNames.getProperty(mCurrentSite[3]);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -84,32 +149,26 @@ class DownloadURLsParserCallback extends ParserCallback
 
     public void handleStartTag(Tag t, MutableAttributeSet a, int pos)
     {
-        if (mSearching) {
-            if(t.equals(Tag.TABLE)) {
-                if(a.isDefined(Attribute.ID)) {
-                    if(Common.stringsAreEqual(ID_MIRRORS,(String)a.getAttribute(Attribute.ID),true)) {
-                        mExpectedTD = TD_HOST;
+        if(!mDone) {
+            if(t.equals(Tag.FORM)) {
+                if(!mInForm) {
+                    if(a.isDefined(Attribute.ACTION)) {
+                        String action = (String)a.getAttribute(Attribute.ACTION);
+                        mInForm = FORM_ACTION.equalsIgnoreCase(action);
                     }
                 }
             }
-            else if(t.equals(Tag.TR)) {
-                mCurrentSite = new String[COUNT];
-            }
-            else if(t.equals(Tag.TD) && mExpectedTD != TD_NONE) {
-                if(a.isDefined(Attribute.COLSPAN)) {
-                    mExpectedTD = TD_NONE;
-                    mSearching = false;
-                }
-                else {
-                    mInTD = true;
+            else if(t.equals(Tag.SPAN)) {
+                if(mInForm) {
+                    mInSpan = true;
                 }
             }
-            else if(mInTD && mExpectedTD == TD_DOWNLOAD && t.equals(Tag.A)) {
-                if(a.isDefined(Attribute.HREF)) {
-                    String url = (String)a.getAttribute(Attribute.HREF);
-                    Matcher matcher = mPattern.matcher(url);
-                    if(matcher.matches()) {
-                        mCurrentSite[TD_DOWNLOAD] = matcher.group(1);
+            else if(t.toString().equals(TAG_LABEL)) {
+                if(mInForm) {
+                    mInLabel = true;
+                    if(mContinent != null) {
+                        mCurrentSite = new String[4];
+                        mCurrentSite[2] = mContinent;
                     }
                 }
             }
@@ -118,9 +177,22 @@ class DownloadURLsParserCallback extends ParserCallback
 
     public void handleText(char[] data, int pos)
     {
-        if (mSearching && mInTD) {
-            if(mExpectedTD == TD_LOCATION || mExpectedTD == TD_CONTINENT) {
-                mCurrentSite[mExpectedTD] = new String(data);
+        if(!mDone) {
+            if(mInForm) {
+                String string = new String(data).trim();
+                if(mInSpan) {
+                    if(!AUTO_SELECT.equalsIgnoreCase(string)) {
+                        mContinent = string;
+                    }
+                    else {
+                        mContinent = null;
+                    }
+                }
+                else if(mInLabel) {
+                    if(mCurrentSite != null) {
+                        mCurrentSite[1] = string;
+                    }
+                }
             }
         }
     }
