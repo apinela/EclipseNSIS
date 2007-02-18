9805096f997473dc2f5b98a23d6eb8b864a8d60b
icemank <>
2007-02-18 13:32:18 +0000

    no message

--- net.sf.eclipsensis/help/help.js
+++ net.sf.eclipsensis/help/help.js
@@ -85,4 +85,24 @@ function changeCursor(id, cur)
     }
 }
 
+function getQueryVariable(variable)
+{
+  var query = document.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+  return null;
+}
+
+function checkHighlight()
+{
+    var id = getQueryVariable("id");
+    if(id) {
+        highlightDef(id);
+    }
+}
 // -->
